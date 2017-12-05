c9a47bf37f296e4132ac8fcfdf926ca58796e9e0
tszhonglau <lauxx265@morris.umn.edu>
2017-12-05 14:20:19 -0600

    Completed pre-lab 8 with comments

--- src/search/ThreadedSearch.java
+++ src/search/ThreadedSearch.java
@@ -27,31 +27,50 @@ public class ThreadedSearch<T> implements Runnable {
   * You can assume that the list size is divisible by `numThreads`
   */
   public boolean parSearch(int numThreads, T target, ArrayList<T> list) throws InterruptedException {
-    /*
-    * First construct an instance of the `Answer` inner class. This will
-    * be how the threads you're about to create will "communicate". They
-    * will all have access to this one shared instance of `Answer`, where
-    * they can update the `answer` field inside that instance.
-    *
-    * Then construct `numThreads` instances of this class (`ThreadedSearch`)
-    * using the 5 argument constructor for the class. You'll hand each of
-    * them the same `target`, `list`, and `answer`. What will be different
-    * about each instance is their `begin` and `end` values, which you'll
-    * use to give each instance the "job" of searching a different segment
-    * of the list. If, for example, the list has length 100 and you have
-    * 4 threads, you would give the four threads the ranges [0, 25), [25, 50),
-    * [50, 75), and [75, 100) as their sections to search.
-    *
-    * You then construct `numThreads`, each of which is given a different
-    * instance of this class as its `Runnable`. Then start each of those
-    * threads, wait for them to all terminate, and then return the answer
-    * in the shared `Answer` instance.
-    */
-    return false;
+    	 /*
+    	* First construct an instance of the `Answer` inner class. This will
+    	* be how the threads you're about to create will "communicate". They
+    	* will all have access to this one shared instance of `Answer`, where
+    	* they can update the `answer` field inside that instance.
+    	*/
+
+ 	Answer answer = new Answer();
+	/* Then construct `numThreads` instances of this class (`ThreadedSearch`)
+    	* using the 5 argument constructor for the class. You'll hand each of
+    	* them the same `target`, `list`, and `answer`. What will be different
+    	* about each instance is their `begin` and `end` values, which you'll
+    	* use to give each instance the "job" of searching a different segment
+    	* of the list. If, for example, the list has length 100 and you have
+    	* 4 threads, you would give the four threads the ranges [0, 25), [25, 50),
+    	* [50, 75), and [75, 100) as their sections to search.
+    	*/
+    	Thread[] threads = new Thread[numThreads];
+    	int sizeSect = list.size()/numThreads;
+    	for (int i = 0; i < numThreads; i++){
+		int startIndex = (i * sizeSect);
+		int endIndex = startIndex + sizeSect;
+    		threads[i] = new Thread (new ThreadedSearch(target, list, startIndex, endIndex, answer));
+		threads[i].start();
+    	}
+	/* You then construct `numThreads`, each of which is given a different
+    	* instance of this class as its `Runnable`. Then start each of those
+    	* threads, wait for them to all terminate, and then return the answer
+    	* in the shared `Answer` instance.
+    	*/
+    	for (int i=0; i < numThreads; i++){
+    		threads[i].join();
+    	}
+    	return answer.getAnswer();
   }
 
+  //Linear search for each thread, once found the target, set answer to true and break
   public void run() {
-
+    for(int i = begin; i < end; i++) {
+    	if(list.get(i).equals(target)) {
+		answer.setAnswer(true);
+		return;
+	}
+    }
   }
 
   private class Answer {
